d2d0835caba9116dba8583033aa9ae721ac09fd1
Seann Alexander <self@seannalexander.com>
2020-05-27 01:22:10 -0400

    add columns
    Added sqlserver.client_hostname and sqlserver.database_name

--- Extended-Events/Queries-Over-1-Second.sql
+++ Extended-Events/Queries-Over-1-Second.sql
@@ -39,10 +39,10 @@ DECLARE @ExtendedEventsName_Today VARCHAR(255) = @Datestring_Today + ''_'' + @Ex
 DECLARE @Exec_CreateEventSession VARCHAR(4000) = ''
 CREATE EVENT SESSION [''+@ExtendedEventsName_Today+''] ON SERVER 
 ADD EVENT sqlserver.module_end(
-    ACTION(sqlos.task_time,sqlserver.client_app_name,sqlserver.database_id,sqlserver.query_hash,sqlserver.session_id,sqlserver.sql_text)
+    ACTION(sqlos.task_time,sqlserver.client_app_name,sqlserver.database_id,sqlserver.query_hash,sqlserver.session_id,sqlserver.sql_text,sqlserver.client_hostname,sqlserver.database_name)
     WHERE ([package0].[greater_than_uint64]([sqlserver].[database_id],(4)) AND [package0].[equal_boolean]([sqlserver].[is_system],(0)) AND [duration]>=(1000000))),
 ADD EVENT sqlserver.rpc_completed(SET collect_statement=(1)
-    ACTION(sqlos.task_time,sqlserver.client_app_name,sqlserver.database_id,sqlserver.query_hash,sqlserver.session_id,sqlserver.sql_text)
+    ACTION(sqlos.task_time,sqlserver.client_app_name,sqlserver.database_id,sqlserver.query_hash,sqlserver.session_id,sqlserver.sql_text,sqlserver.client_hostname,sqlserver.database_name)
     WHERE ([package0].[greater_than_uint64]([sqlserver].[database_id],(4)) AND [package0].[equal_boolean]([sqlserver].[is_system],(0)) AND [duration]>=(1000000))) 
 ADD TARGET package0.event_file(SET filename=N'''''' + @ExtendedEvents_FilePath + ''\''+@ExtendedEventsName_Today+''.xel'''',max_file_size=(256),max_rollover_files=(10))
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=ON,STARTUP_STATE=ON)
